6d4b61e481e18a8fea76ce09620f6f05864b7050
KomoreKalu <komorekalu@outlook.com>
2021-03-16 20:55:03 +0800

    feat: add Qmsg

--- forward.sh
+++ forward.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 ID= #chat_id
 TOKEN= #token
-LOOP=7
+LOOP=3
 DATE=$(date '+%Y/%m/%d %H:%M')
 if [ "$1" == "RECEIVED" ]; then
 	FROM=$(sed -n '1p' < "$2" | awk -F ': ' '{printf $2}')
 	BODY=$(sed -e '1,/^$/ d' < "$2" | iconv -f UNICODEBIG -t UTF-8)
 	CONTENT="新短信%20$DATE%0a来自:%20$FROM%0a$BODY"
-	echo -e "$DATE\n$FROM\n$BODY\n" >> /root/sms/sms.log
+	echo -e "$DATE\n$FROM\n$BODY\n" >> /root/Messenger/sms.log
 fi
 
-vurp(){
-	CODE=$(curl -o /dev/null -s -w "%{http_code}" https://www.google.com -x socks5://172.16.7.1:3077 --connect-timeout 10)
-	if [ "$CODE" == "200" ]; then
-		curl -d "chat_id=$ID&text=$CONTENT" -X POST https://api.telegram.org/bot$TOKEN/sendMessage -x socks5://172.16.7.1:3077
-	elif [ $LOOP -gt 0 ]; then
-		echo "proxy failure at $(date '+%Y/%m/%d %H:%M')"
+toQmsg(){
+	curl -d "qq=$ID&msg=$CONTENT" https://qmsg.zendee.cn/send/${TOKEN} --retry 3 --retry-delay 5 
+}
+
+toTG(){
+	curl -d "chat_id=$ID&text=$CONTENT" -X POST https://api.telegram.org/bot${TOKEN}/sendMessage --retry 3 --retry-delay 5 
+}
+
+forward(){
+	toQmsg
+	#toTG
+	if [ $? -ne 0 -a $LOOP -gt 0 ]; then
+		echo "failure at $(date '+%Y/%m/%d %H:%M')"
 		((LOOP--))
-		sleep 300	
-		vurp
+		sleep 60
+		forward
 	fi
 }
 
-vurp
+forward
